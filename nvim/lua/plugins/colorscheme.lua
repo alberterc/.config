e8f7d5d4bc78a0a5f55aab0d287d52f9331e7eed
alberterc <85069947+alberterc@users.noreply.github.com>
2025-01-04 21:24:00 +0700

    feat: add window transparency

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,13 @@ return {
   "rebelot/kanagawa.nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme("kanagawa-dragon")
+    local function setColor(color)
+      vim.cmd.colorscheme(color or "kanagawa-dragon")
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+
+    setColor()
   end,
 }

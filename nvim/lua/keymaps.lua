de0ec518b40321f4306c26f90336db35c96146aa
alberterc <85069947+alberterc@users.noreply.github.com>
2025-01-04 22:30:02 +0700

    feat: change nvim tree toggle keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -18,4 +18,4 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.api.nvim_set_keymap("n", "<leader>h", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle, { desc = "Toggle File Tree" })
